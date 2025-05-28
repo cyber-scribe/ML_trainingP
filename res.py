ab7b580f547d769ea57331e0c5972961c7ed6556
cyber-scribe <vd297520@gmail.com>
2025-05-28 22:10:19 +0530

    changes

--- res.py
+++ res.py
@@ -9,3 +9,7 @@ button =st.button("Predict the Score!!")
 if(button):
     model=pickle.load(open("res.pkl","rb"))
     res = model.predict([[sh]])[0]
+    if(sh> 10):
+        st.markdown(f"Invalid input")
+    else:
+        st.markdown(f"Predicted Score is : {res}")
